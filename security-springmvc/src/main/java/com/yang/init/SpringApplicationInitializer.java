b0916b53e1c888ce04af066e9fa1117132bbe922
yangchenglin <yangchenglin5533@163.com>
2021-02-24 21:21:41 +0800

    111

---
+++ security-springmvc/src/main/java/com/yang/init/SpringApplicationInitializer.java
@@ -0,0 +1,25 @@
+package com.yang.init;
+
+import com.yang.conf.ApplicationConfig;
+import com.yang.conf.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Administrator
+ */
+public class SpringApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
